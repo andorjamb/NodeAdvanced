0c940cd6dcef22f0bac95b54ad89def331665a00
Anna Petelin <andorjamb@protonmail.com>
2023-01-17 16:01:27 +0300

    end of lesson week 2, first mariabd and mySql servers

---
+++ database-project/testWithFunctions.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const Database = require("./database");
+
+const db = new Database(options);
+
+//functions
+
+async function getAll() {
+    try{
+        const result = 
+
+    }
+}
+
+
+//main function
+async function run(){
+    await getAll();
+    await getAll();
+}
